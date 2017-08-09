79b44b7b2e6c7fd3fd003dfa9e5957cc4f5b6d1b
Rezky Aulia Pratama <rezkyauliapratama@gmail.com>
2017-08-09 18:06:36 +0700

    - Version 0.0.0.3

--- app/src/main/java/com/rezkyaulia/android/popular_movie/BaseApplication.java
+++ app/src/main/java/com/rezkyaulia/android/popular_movie/BaseApplication.java
@@ -3,7 +3,9 @@ package com.rezkyaulia.android.popular_movie;
 import android.app.Application;
 
 import com.androidnetworking.AndroidNetworking;
+import com.app.infideap.stylishwidget.view.Stylish;
 import com.rezkyaulia.android.popular_movie.database.DbHelper;
+import com.rezkyaulia.android.popular_movie.util.PreferencesManager;
 
 import timber.log.Timber;
 
@@ -17,6 +19,16 @@ public class BaseApplication extends Application {
         super.onCreate();
         Timber.plant(new Timber.DebugTree());
         AndroidNetworking.initialize(getApplicationContext());
+        PreferencesManager.init(this);
+
+        String fontFolder = "fonts/Exo_2/Exo2-";
+        Stylish.getInstance().set(
+                fontFolder.concat("Regular.ttf"),
+                fontFolder.concat("Medium.ttf"),
+                fontFolder.concat("RegularItalic.ttf")
+        );
+
+        Stylish.getInstance().setFontScale(1.0f);
 
     }
 }
